1c770f6fafc0595a4cb5beb9a5077174d9bd7c11
Showrin Barua <showrinece15@gmail.com>
2020-07-18 02:33:59 +0600

    Create flex-container component

---
+++ src/Flexo/components/FlexContainer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FlexContainer = (props) => {
+	return <div className="flex-container"></div>;
+};
+
+export default FlexContainer;
