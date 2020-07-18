04bda636287b37deb77153c3ad5299da27f48def
Showrin Barua <showrinece15@gmail.com>
2020-07-18 22:11:19 +0600

    Handle mainAxis, crossAxis, padding using states

--- src/Flexo/components/FlexContainer.jsx
+++ src/Flexo/components/FlexContainer.jsx
@@ -1,26 +1,44 @@
 import React from 'react';
 import Axis from './Axis';
+import Consumer from '../../contextSetup';
 import FlexChild from './FlexChild';
 
 const FlexContainer = (props) => {
 	return (
-		<div className="flex-container">
-			<Axis direction="horizontal" type="main" />
-			<Axis direction="vertical" type="cross" />
-			<FlexChild id={1} />
-			<FlexChild id={2} />
-			<FlexChild id={3} />
-			<FlexChild id={4} isSelected={true} />
-			<FlexChild id={5} />
-			<FlexChild id={6} />
-			<FlexChild id={7} />
-			<FlexChild id={8} />
-			<FlexChild id={9} />
-			<FlexChild id={10} />
-			<FlexChild id={11} />
-			<FlexChild id={12} />
-			<FlexChild id={13} />
-		</div>
+		<Consumer>
+			{(context) => {
+				const {
+					showMainAxis,
+					showCrossAxis,
+					addPadding,
+				} = context.appState;
+				const padding = addPadding ? '50px 88px' : '0';
+
+				return (
+					<div className="flex-container" style={{ padding }}>
+						{showMainAxis && (
+							<Axis direction="horizontal" type="main" />
+						)}
+						{showCrossAxis && (
+							<Axis direction="vertical" type="cross" />
+						)}
+						<FlexChild id={1} />
+						<FlexChild id={2} />
+						<FlexChild id={3} />
+						<FlexChild id={4} isSelected={true} />
+						<FlexChild id={5} />
+						<FlexChild id={6} />
+						<FlexChild id={7} />
+						<FlexChild id={8} />
+						<FlexChild id={9} />
+						<FlexChild id={10} />
+						<FlexChild id={11} />
+						<FlexChild id={12} />
+						<FlexChild id={13} />
+					</div>
+				);
+			}}
+		</Consumer>
 	);
 };
 
