c4893bb8ea346774efac450f3ddf318bac461893
Showrin Barua <showrinece15@gmail.com>
2020-07-18 22:10:24 +0600

    Setup state variables and state handlers for components in bottombar

--- src/contextSetup.jsx
+++ src/contextSetup.jsx
@@ -2,9 +2,42 @@ import React, { createContext, useState } from 'react';
 const { Consumer, Provider } = createContext();
 
 const ContextProvider = (props) => {
-	const [appState, setAppState] = useState({});
+	const [appState, setAppState] = useState({
+		showMainAxis: true,
+		showCrossAxis: true,
+		addPadding: true,
+	});
 
-	return <Provider value={appState}>{props.children}</Provider>;
+	const handleMainAxisToggle = () =>
+		setAppState((preState) => ({
+			...preState,
+			showMainAxis: !preState.showMainAxis,
+		}));
+
+	const handleCrossAxisToggle = () =>
+		setAppState((preState) => ({
+			...preState,
+			showCrossAxis: !preState.showCrossAxis,
+		}));
+
+	const handlePaddingToggle = () =>
+		setAppState((preState) => ({
+			...preState,
+			addPadding: !preState.addPadding,
+		}));
+
+	return (
+		<Provider
+			value={{
+				appState,
+				handleMainAxisToggle,
+				handleCrossAxisToggle,
+				handlePaddingToggle,
+			}}
+		>
+			{props.children}
+		</Provider>
+	);
 };
 
 export { ContextProvider };
