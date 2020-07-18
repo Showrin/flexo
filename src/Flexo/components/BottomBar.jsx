0b052d5a26ba21d6d9a497655c28ad13b4878c80
Showrin Barua <showrinece15@gmail.com>
2020-07-18 22:12:18 +0600

    Implement interaction between state and components in bottombar

--- src/Flexo/components/BottomBar.jsx
+++ src/Flexo/components/BottomBar.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
+import Consumer from '../../contextSetup';
 import Toggle from './Toggle';
 
 const BottomBar = () => {
 	return (
-		<div className="bottom-bar">
-			<Toggle label="Main Axis" toggleColor="danger" />
-			<Toggle label="Cross Axis" toggleColor="success" />
-			<Toggle label="Padding" toggleColor="black" />
-		</div>
+		<Consumer>
+			{(context) => {
+				const {
+					handleMainAxisToggle,
+					handleCrossAxisToggle,
+					handlePaddingToggle,
+				} = context;
+
+				return (
+					<div className="bottom-bar">
+						<Toggle
+							label="Main Axis"
+							toggleColor="danger"
+							onClick={handleMainAxisToggle}
+						/>
+						<Toggle
+							label="Cross Axis"
+							toggleColor="success"
+							onClick={handleCrossAxisToggle}
+						/>
+						<Toggle
+							label="Padding"
+							toggleColor="black"
+							onClick={handlePaddingToggle}
+						/>
+					</div>
+				);
+			}}
+		</Consumer>
 	);
 };
 
