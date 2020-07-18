3895fad1088c8e88f52c472c1dd61b9834e3c8e8
Showrin Barua <showrinece15@gmail.com>
2020-07-18 12:06:59 +0600

    Add Axis component in the component registry

--- src/Flexo/components/index.js
+++ src/Flexo/components/index.js
@@ -2,3 +2,4 @@ export { default as Navbar } from './Navbar';
 export { default as FlexContainer } from './FlexContainer';
 export { default as BottomBar } from './BottomBar';
 export { default as Toggle } from './Toggle';
+export { default as Axis } from './Axis';
