5aed884e8aa9470cf7a83289f504e9a84600c873
Showrin Barua <showrinece15@gmail.com>
2020-07-18 11:35:17 +0600

    Add Toggle components in the bottombar

--- src/Flexo/components/BottomBar.jsx
+++ src/Flexo/components/BottomBar.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Toggle from './Toggle';
 
 const BottomBar = () => {
-	return <div className="bottom-bar"></div>;
+	return (
+		<div className="bottom-bar">
+			<Toggle label="Main Axis" toggleColor="danger" />
+			<Toggle label="Cross Axis" toggleColor="success" />
+			<Toggle label="Padding" toggleColor="black" />
+		</div>
+	);
 };
 
 export default BottomBar;
