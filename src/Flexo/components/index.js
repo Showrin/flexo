ddfabcb853538a922a57c4898a8d37069d5f9f7c
Showrin Barua <showrinece15@gmail.com>
2020-07-18 11:34:48 +0600

    Add Toggle component in the component registry

--- src/Flexo/components/index.js
+++ src/Flexo/components/index.js
@@ -1,3 +1,4 @@
 export { default as Navbar } from './Navbar';
 export { default as FlexContainer } from './FlexContainer';
 export { default as BottomBar } from './BottomBar';
+export { default as Toggle } from './Toggle';
