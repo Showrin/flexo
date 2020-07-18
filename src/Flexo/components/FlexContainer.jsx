79a09a88e4a30c4fd2d1b3667751ed89dc86330e
Showrin Barua <showrinece15@gmail.com>
2020-07-18 17:59:14 +0600

    Add FlexChild components in the flexContainer

--- src/Flexo/components/FlexContainer.jsx
+++ src/Flexo/components/FlexContainer.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import Axis from './Axis';
+import FlexChild from './FlexChild';
 
 const FlexContainer = (props) => {
 	return (
 		<div className="flex-container">
 			<Axis direction="horizontal" type="main" />
 			<Axis direction="vertical" type="cross" />
+			<FlexChild id={1} />
+			<FlexChild id={2} />
+			<FlexChild id={3} />
+			<FlexChild id={4} isSelected={true} />
+			<FlexChild id={5} />
+			<FlexChild id={6} />
+			<FlexChild id={7} />
+			<FlexChild id={8} />
+			<FlexChild id={9} />
+			<FlexChild id={10} />
+			<FlexChild id={11} />
+			<FlexChild id={12} />
+			<FlexChild id={13} />
 		</div>
 	);
 };
