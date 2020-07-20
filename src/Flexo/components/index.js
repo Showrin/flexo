56a188c932ea8b907399139298e2e619e74d7ee3
Showrin Barua <showrinece15@gmail.com>
2020-07-20 07:43:05 +0600

    Add siderbar in the component registry

--- src/Flexo/components/index.js
+++ src/Flexo/components/index.js
@@ -4,3 +4,4 @@ export { default as BottomBar } from './BottomBar';
 export { default as Toggle } from './Toggle';
 export { default as Axis } from './Axis';
 export { default as FlexChild } from './FlexChild';
+export { default as Sidebar } from './Sidebar';
