fac7754c52b2b35a3589076f9f427b7809493cdd
Showrin Barua <showrinece15@gmail.com>
2020-07-22 21:22:51 +0600

    Close sidebar if navbar is clicked

--- src/Flexo/components/Navbar.jsx
+++ src/Flexo/components/Navbar.jsx
@@ -1,33 +1,47 @@
 import React from 'react';
+import Consumer from '../../contextSetup';
 import { ReactComponent as FlexoLogo } from '../assets/logo.svg';
 import creatorAvatar from '../assets/showrinAvatar.png';
 
 const Navbar = () => {
 	return (
-		<nav className="navbar">
-			<div className="navbar__logo">
-				<FlexoLogo />
-			</div>
-			<div className="navbar__creator">
-				<div className="navbar__creator-info">
-					<div className="navbar__creator-info-text">Created By</div>
-					<div className="navbar__creator-info-name">
-						Showrin Barua
-					</div>
-					<a
-						className="navbar__creator-info-link"
-						href="https://www.showrin.com"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Visit Creator's Site
-					</a>
-				</div>
-				<div className="navbar__creator-avatar">
-					<img src={creatorAvatar} alt="Avatar for Showrin" />
-				</div>
-			</div>
-		</nav>
+		<Consumer>
+			{(context) => {
+				const { closeSidebar } = context;
+
+				return (
+					<nav className="navbar" onClick={closeSidebar}>
+						<div className="navbar__logo">
+							<FlexoLogo />
+						</div>
+						<div className="navbar__creator">
+							<div className="navbar__creator-info">
+								<div className="navbar__creator-info-text">
+									Created By
+								</div>
+								<div className="navbar__creator-info-name">
+									Showrin Barua
+								</div>
+								<a
+									className="navbar__creator-info-link"
+									href="https://www.showrin.com"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									Visit Creator's Site
+								</a>
+							</div>
+							<div className="navbar__creator-avatar">
+								<img
+									src={creatorAvatar}
+									alt="Avatar for Showrin"
+								/>
+							</div>
+						</div>
+					</nav>
+				);
+			}}
+		</Consumer>
 	);
 };
 
