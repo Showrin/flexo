989d79d694bfe2b4a5a3ed057ee8202b9afeabfd
Showrin <showrinece15@gmail.com>
2020-07-25 20:30:25 +0600

    Setup firebase configuration

---
+++ src/dbConfig.js
@@ -0,0 +1,12 @@
+const dbConfig = {
+	apiKey: 'AIzaSyCFi-2DbpatLZNatIAzkGSnoLxe5PYsoLE',
+	authDomain: 'flexo-app.firebaseapp.com',
+	databaseURL: 'https://flexo-app.firebaseio.com',
+	projectId: 'flexo-app',
+	storageBucket: 'flexo-app.appspot.com',
+	messagingSenderId: '788115999137',
+	appId: '1:788115999137:web:e68d6fa80031948d21a3e0',
+	measurementId: 'G-WWVS7E90DJ',
+};
+
+export default dbConfig;
